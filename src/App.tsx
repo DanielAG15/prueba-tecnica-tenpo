31df110e9312aa0df7045bfb627c1c4a1fe3bd15
Daniel Anzoategui Gomez <daniel.anzoategui@aossas.com>
2025-03-05 17:12:01 -0500

    fix DarkMode

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import Login from "./pages/login";
 import Home from "./pages/Home";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { JSX } from "react";
 
 const PrivateRoute = ({ element }: { element: JSX.Element }) => {
@@ -21,13 +23,24 @@ const PublicRoute = ({ element }: { element: JSX.Element }) => {
 };
 
 const App = () => {
+  const darkMode = useSelector((state: RootState) => state.theme.darkMode);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PublicRoute element={<Login />} />} />
-        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicRoute element={<Login />} />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
